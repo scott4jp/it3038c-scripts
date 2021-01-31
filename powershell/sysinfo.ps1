7c90812501b2c8367c11747205f6ba3e9196f1fe
unknown <scott4jp@mail.uc.edu>
2021-01-30 23:43:52 -0500

    adding pwershellfiles

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,12 @@
+﻿function getIP {
+    (Get-NetIPAddress).IPv4Address | Select-String "192*"
+    }
+
+write-host(getIP)
+$IP = getIP
+$Date = Get-Date
+$Body = "This machine's IP is $IP. User is $env:username. Hostname is $. PowerShell Version. Todays date is $DATE"
+
+write-host($Body)
+
+#Send-MailMessage -To "jscott017@outlook.com" -From "jsmail027@gmail.com" -Subject "IT3038C Windows Sysinfo" -Body $Body -SmtpServer smtp.google.com -port 587 -UseSSL -Credential (Get-Credential)
