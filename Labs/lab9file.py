1f72c7d749fe1a28f692cb4cee506f1d409e74c4
James Scott <scott4jp@mail.uc.edu>
2021-04-11 20:45:54 -0400

    new

--- Labs/lab9file.py
+++ Labs/lab9file.py
@@ -5,7 +5,21 @@ r = requests.get('http://localhost:3000')
 
 data = r.json()
 
-print(data)
+first =(data[0]["name"])
+first_ = (data[0]["color"])
+print(first + " is " + first_)
+
+second =(data[1]["name"])
+second_ = (data[1]["color"])
+print(second + " is " + second_)
+
+third =(data[2]["name"])
+third_ = (data[2]["color"])
+print(third + " is " + third_)
+
+fourth =(data[3]["name"])
+fourth_ = (data[3]["color"])
+print(fourth + " is " + fourth_)
 
 #Widget is blue
 #Widget is green
